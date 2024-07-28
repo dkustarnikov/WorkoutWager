03c8decd7c5d3514a5c69043d7ef16c89d0ff250
Dmitry Kustarnikov <dmitry.kustarnikov@gmail.com>
2024-07-28 09:40:31 -0400

    fix(workflow): Fixing the workflow scripts

--- src/lambdas/health/index.ts
+++ src/lambdas/health/index.ts
@@ -4,5 +4,5 @@ import { getApiResponse } from '../../common/helpers';
 export const handler: awsLambda.Handler = async () => {
   const someKey = process.env.SOME_KEY;
   console.log(`SOME_KEY: ${someKey}`);
-  return getApiResponse(200, JSON.stringify({ message: 'Healthy!' }));
+  return getApiResponse(200, JSON.stringify({ message: 'Healthy! Yay!' }));
 };
